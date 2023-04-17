a46749aab6e8d63673026bb2eede952ec6c33e84
Daniel Arao <araodaniel14@gmail.com>
2023-04-17 10:13:48 -0400

    form ready

--- app_crud/form.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class CreateNew(forms.Form):
-    title= forms.CharField(label="title", max_length=50)
-    description = forms.CharField(label="description", widget=forms.Textarea)
--- app_crud/views.py
+++ app_crud/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render, redirect
 from .models import Task
-from .form import CreateNew
+
 
 #04p161abs no arranca en manual
 #04v104
-createnew = CreateNew()
+
 # Create your views here.
 
     
 def create_task (request):
-    if request.method == 'GET':
-        return render(request, 'index.html',
-                      {
-                        'form':createnew
-                      }
-                      )
-    else:
-        Task.objects.create(
-            title = request.POST['title'],
-            description = request.POST['description']
+    new_title = request.POST["title"]
+    new_description = request.POST["description"]
+    if new_title == "" or new_description == "":
+        tasks = Task.objects.all()
+        return render(
+            request, "index.html", {"tasks": tasks, "error": "Title and description is required"}
         )
-        return redirect('/')
+    task = Task(title=new_title, description=new_description)
+    task.save()
+    return redirect("/")
     
 def index(request):
     tasks = Task.objects.all()
